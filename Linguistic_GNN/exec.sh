19861dfc549c1faefb7fa2539b9aea5d4b8c29ff
yannick <yynzeuhang@gmail.com>
2024-03-19 15:11:49 +0100

    ok

--- Linguistic_GNN/exec.sh
+++ Linguistic_GNN/exec.sh
@@ -1,11 +1,11 @@
-python data_processing.py --input_folder '' --number_sentence 5000
+python3 data_processing.py --input_folder '' --number_sentence 5000
 
-python word2vec_model.py --input_folder 
+python3 word2vec_model.py --input_folder 
 
-python build_graph.py --input_folder ''
+python3 build_graph.py --input_folder ''
 
-python w2v_vs_gnn.py --input_folder '' --model 'w2v'
+python3 w2v_vs_gnn.py --input_folder '' --model 'w2v'
 
-python w2v_vs_gnn.py --input_folder '' --model 'gnn'
+python3 w2v_vs_gnn.py --input_folder '' --model 'gnn'
 
 
