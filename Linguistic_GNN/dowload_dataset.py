56e13679ae383c09da643fd4188891120ab21d3f
yannick <yynzeuhang@gmail.com>
2024-04-10 12:58:09 +0100

    ok

--- Linguistic_GNN/dowload_dataset.py
+++ Linguistic_GNN/dowload_dataset.py
@@ -9,6 +9,6 @@ args = parser.parse_args()
 input_folder = args.input_folder
 
 data = load_dataset("wikipedia", "20220301.simple")
-dataset= data['train']['text']
+
 save_pickle(data=dataset, file_path=os.path.join(input_folder,'wiki235.pkl'))
 
