93c39d99baf2eaa141931cc6835b96f28c766bed
yannick <yynzeuhang@gmail.com>
2024-04-10 12:53:42 +0100

    ok

--- Linguistic_GNN/dowload_dataset.py
+++ Linguistic_GNN/dowload_dataset.py
@@ -1,5 +1,11 @@
 from datasets import load_dataset
 from data_processing import load_pickle, save_pickle
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_folder', help ='source folder')
+args = parser.parse_args()
+input_folder = args.input_folder
 
 data = load_dataset("wikipedia", "20220301.simple")
 dataset= data['train']['text']
